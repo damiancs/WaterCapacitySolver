a32033eacdb07e2822c1c605e5d62e134bb41e2f
Sebastian Damian <damiancs@yahoo.com>
2017-03-15 18:59:43 +0200

    Create correct directory structure for a valid python package.

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-"""
-Init file for WaterCapacitySolver module.
-"""
-
-from WaterCapacitySolver.solver import Solver
-from WaterCapacitySolver.errors import SolverError
-
-__all__ = ["Solver", "SolverError"]
